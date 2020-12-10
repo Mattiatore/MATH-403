a5b35d496e163422ee5b3086b5e8582191a070a5
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-10 02:18:40 +0100

    Add files via upload
    added variable n

--- task2.m
+++ task2.m
@@ -1,7 +1,8 @@
-F =zeros(50,50,50);
-for i=1:50
-    for j=1:50
-        for k=1:50
+n=99
+F =zeros(n,n,n);
+for i=1:n
+    for j=1:n
+        for k=1:n
             F(i,j,k) = 1/(i/10+j/10+k/10)^(1/2);
         end
     end
