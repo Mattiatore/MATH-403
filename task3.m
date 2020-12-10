07217d57fd7591ef040ae656e831d8102b9d73d2
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-10 15:33:48 +0100

    Add files via upload
    task 3

--- task3.m
+++ task3.m
@@ -1,33 +1,33 @@
-clear all
-size_tensor = 70; % try also 50, HOSVD seems terrible, also i did not try 200 was too slow.
-X = rand(size_tensor, size_tensor, size_tensor);
-T1 = zeros(10,1);
-T2 = zeros(10,1);
-N1 = zeros(10,1);
-N2 = zeros(10,1);
-eps = 0.0001;
-for k = 5:5:50
-    X = tensor(X);
-    tic
-    [C,U1,U2,U3] =  mHOSVD(X,10,k,k,k,eps);
-    T1(k/5) = toc;
-    diff = X - ttm(C,{U1,U2,U3},[1 2 3]);
-    N1(k/5) = norm(diff); 
-    tic
-    T = hosvd(X,eps,'ranks',[k,k,k]);
-    T2(k/5)= toc;
-    diff = X - ttm(T.core,{T.U{1},T.U{2},T.U{3}},[1 2 3]);
-    N2(k/5) = norm(X-T); 
-end
+clc; clear; close all
+
+size_tensor = 200;
+X = tensor(normrnd(0,1, size_tensor, size_tensor, size_tensor));
+ranks = 5:5:50;
+
+% HOSVD
+[errHOSVD, timeHOSVD] = testHOSVD(X, 0, ranks, 0, 1);
+
+% MHOSVD
+[errMHOSVD, timeMHOSVD] = testMHOSVD(X, 0, ranks, eps, 0, 1);
+
 figure(1)
-semilogy(5:5:50,N1)
-legend("mHOSVD error")
-figure(2)
-semilogy(5:5:50,N2)
-legend("HOSVD error")
-figure(3)
-plot(5:5:50,T1)
+semilogy(ranks, errMHOSVD, 'k-o', 'markersize', 9)
 hold on
-plot(5:5:50,T2)
+semilogy(ranks, errHOSVD, 'r-d', 'markersize', 9)
+xlabel('Rank')
+ylabel('Relative error')
+box on
+legend({'MHOSVD','HOSVD'}, 'location', 'NE')
+title("Error comparison for random tensor of MHOSVD vs HOSVD")
 hold off
-legend("mHOSVD time","HOSVD time")
+
+figure(2)
+semilogy(ranks, timeMHOSVD, 'k-o', 'markersize', 9)
+hold on
+semilogy(ranks, timeHOSVD, 'r-d', 'markersize', 9)
+xlabel('Rank')
+ylabel('Time cost')
+box on
+legend({'MHOSVD','HOSVD'}, 'location', 'NE')
+title("Time comparison for random tensor of MHOSVD vs HOSVD")
+hold off
