cecc7415c5c8005ec34f9b087f86f4125c3842bd
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2020-12-10 15:41:18 +0100

    Update task3.m
    change

--- task3.m
+++ task3.m
@@ -1,6 +1,6 @@
 clc; clear; close all
 
-size_tensor = 200;
+size_tensor = 200; % try also 100
 X = tensor(normrnd(0,1, size_tensor, size_tensor, size_tensor));
 ranks = 5:5:50;
 
@@ -8,7 +8,7 @@ ranks = 5:5:50;
 [errHOSVD, timeHOSVD] = testHOSVD(X, 0, ranks, 0, 1);
 
 % MHOSVD
-[errMHOSVD, timeMHOSVD] = testMHOSVD(X, 0, ranks, eps, 0, 1);
+[errMHOSVD, timeMHOSVD] = testMHOSVD(X, 0, ranks, eps, 25, 1); % change oversampling "25" if using smaller sized tensor
 
 figure(1)
 semilogy(ranks, errMHOSVD, 'k-o', 'markersize', 9)
